4c72c977994c52fcead51d0f9879f7a36095946d
FioFiyo <fioarlego1@msn.com>
2016-07-11 22:31:14 +0000

    added action new song and set method and action for new song form

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,19 @@ get '/songs' do
 	@songs = Song.all
 	erb :'songs/index'
 end
+
+get	'/songs/new_song' do
+	erb :'songs/new_song'
+end
+
+#submiting new data to server, POST request to server
+#telling sinatra to accept this action
+post '/songs' do
+	@song = Song.new(
+		title: params[:title],
+		author: params[:author],
+		url: params[:url]
+		)
+	@song.save
+	redirect '/songs/new_song'
+end
